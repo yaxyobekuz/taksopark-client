248a5a973c7f16e6683f623662ce3175cffc244b
yaxyobekuz <pubgn9642@gmail.com>
2026-05-17 19:25:16 +0500

    feat: remove damages feature including API, components, and hooks

--- src/owner/features/damages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DamagesListPage } from "./pages/DamagesListPage";
-export { useDamagesQuery } from "./hooks/useDamagesQuery";
-export { useDamageCreate, useDamageDelete } from "./hooks/useDamageMutations";
