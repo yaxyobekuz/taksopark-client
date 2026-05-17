287d2fa096f62679d6ba8fbe7eae9066cdd234d9
yaxyobekuz <pubgn9642@gmail.com>
2026-05-17 19:25:26 +0500

    feat: remove fines feature including API, components, and hooks

--- src/owner/features/fines/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FinesListPage } from "./pages/FinesListPage";
-export { useFinesQuery, useFineQuery } from "./hooks/useFinesQuery";
-export { useFineCreate, useFineDelete } from "./hooks/useFineMutations";
