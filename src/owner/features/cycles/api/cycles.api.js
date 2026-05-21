41d538af33623beae4d03585dc1cf97f686a2a33
yaxyobekuz <pubgn9642@gmail.com>
2026-05-21 16:24:56 +0500

    refactor: remove cycles feature components and API for cleanup

--- src/owner/features/cycles/api/cycles.api.js
+++
@@ -1,9 +0,0 @@
-import http from "@/shared/api/http";
-import { ENDPOINTS } from "@/shared/api/endpoints";
-
-export const cyclesAPI = {
-  list: (params) => http.get(ENDPOINTS.cycles.base, { params }),
-  getById: (id) => http.get(ENDPOINTS.cycles.byId(id)),
-  currentForDriver: (driverId) => http.get(ENDPOINTS.cycles.currentForDriver(driverId)),
-  settle: (id) => http.post(ENDPOINTS.cycles.settle(id)),
-};
--- src/owner/features/cycles/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CyclesListPage } from "./pages/CyclesListPage";
-export { useCyclesQuery, useCycleQuery } from "./hooks/useCyclesQuery";
-export { useCycleSettleMutation } from "./hooks/useCycleSettleMutation";
